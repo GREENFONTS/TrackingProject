536bf258982e66f7180f22ff22c4ce1e9cb8d1ee
GREENFONTS <godwillonyewuchii@gmail.com>
2023-11-04 16:01:16 +0100

    fix

--- src/helper.ts
+++ src/helper.ts
@@ -1,39 +1,52 @@
 export function getLocation(key) {
+    if ("geolocation" in navigator) {
+      // Geolocation is available
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          console.log("enetered");
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
 
-  fetch(`https://www.googleapis.com/geolocation/v1/geolocate?key=${key}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ considerIp: "true" }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const location = data.location;
-      const latitude = location.lat;
-      const longitude = location.lng;
-      const SendLocation = async () => {
-        const formBody = {
-          latitude: latitude,
-          longitude: longitude,
-        };
-        try {
-          const res = await fetch("https://gifts-circle.herokuapp.com/api/sms/send", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formBody),
-          });
-          const result = await res.json();
-          console.log(result);
-        } catch (error) {
-          console.log(error);
+          const SendLocation = async () => {
+            const formBody = {
+              latitude: latitude,
+              longitude: longitude,
+            };
+            try {
+              const res = await fetch("https://gifts-circle.herokuapp.com/api/sms/send", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formBody),
+              });
+              const result = await res.json();
+              console.log(result);
+            } catch (error) {
+              console.log(error);
+            }
+          };
+          SendLocation();
+        },
+        function (error) {
+          // Handle errors, if any
+          switch (error.code) {
+            case error.PERMISSION_DENIED:
+              console.error("User denied the request for Geolocation.");
+              break;
+            case error.POSITION_UNAVAILABLE:
+              console.error("Location information is unavailable.");
+              break;
+            case error.TIMEOUT:
+              console.error("The request to get user location timed out.");
+              break;
+            default:
+              console.error("An unknown error occurred.");
+              break;
+          }
         }
-      };
-      SendLocation();
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+      );
+    } else {
+      console.error("Geolocation is not available in this browser.");
+    }
 }
